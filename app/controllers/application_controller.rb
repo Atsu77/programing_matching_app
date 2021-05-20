bc87e772a3ffab17a79afe2f339adc0316bc3505
Atsu <kamnknon@gmail.com>
2021-05-20 09:35:18 +0900

    jqueryを導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
     #nameカラムで保存できるように設定する
-    before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
   
     protected
